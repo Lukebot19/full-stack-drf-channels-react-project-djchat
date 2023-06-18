c13a2b3aa53f74396dc9a611fcbd78d6acc529e7
Lukebot19 <132459699+LukeAlexander1@users.noreply.github.com>
2023-06-19 00:44:49 +0100

    authentication

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #External
+    # External
     "drf_spectacular",
-    #Internal
+    # Internal
     "account",
     "server",
 ]
@@ -135,11 +135,14 @@ AUTH_USER_MODEL = "account.Account"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "DJ Chat App",
-    "DESCRIPTION":"",
+    "DESCRIPTION": "",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": True,
 }
