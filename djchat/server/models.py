e606c58f42f0cba58cf9b85646c76a23f659e8ca
Lukebot19 <132459699+LukeAlexander1@users.noreply.github.com>
2023-06-19 00:33:54 +0100

    update models

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -18,7 +18,7 @@ class Server(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT, related_name="server_category"
     )
-    descripiton = models.CharField(max_length=250, null=True)
+    descripiton = models.CharField(max_length=250, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Channel(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
     )
-    topic = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100, blank=True, null=True)
     server = models.ForeignKey(
         Server, on_delete=models.CASCADE, related_name="channel_server"
     )
