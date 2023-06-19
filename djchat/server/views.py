7b106561da9b7e52d39f5af3c5cace741a9f5947
Lukebot19 <132459699+LukeAlexander1@users.noreply.github.com>
2023-06-19 01:04:52 +0100

    list view

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Server
+from .serializer import ServerSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class ServerListViewSet(viewsets.ViewSet):
+
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        serializer = ServerSerializer(self.queryset, many=True)
+        return Response(serializer.data)
